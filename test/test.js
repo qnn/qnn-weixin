9cf235f401706648815e1fbb9eb1def8b86ed334
caiguanhao <caiguanhao@gmail.com>
2013-09-09 15:02:12 +0800

    add send text coord test; stores_list cache problem still exists;

--- test/test.js
+++ test/test.js
@@ -5,6 +5,8 @@ var request = require('supertest');
 var libweixin = require('../lib/weixin');
 
 var format = require('util').format;
+var assert = require('assert');
+var parseString = require('xml2js').parseString;
 
 require('js-yaml');
 var test = require('./test.yml');
@@ -48,16 +50,35 @@ describe('subscription functionality', function(){
 });
 
 describe('find nearby stores with position/coordinates functionality', function(){
+  var x = 22.849068, y = 113.216673;
+
+  var store = require('../lib/store');
+  // remove stores.json from module caches as its content was manipulated. it will be 're-required'.
+  delete require.cache[require.resolve('../stores.json')];
+  var stores_list = store.flatten(require('../stores.json'));
+  var list = store.find_nearby_stores_to_weixin_list(stores_list, x, y);
+
+  var validate_content = function(content, done) {
+    parseString(content, function (err, result) {
+      if (err) throw err;
+      assert(result.hasOwnProperty('xml'), 'not xml?');
+      assert(result.xml.hasOwnProperty('Articles'), 'no articles?');
+      assert((result.xml.Articles instanceof Array), 'no articles?');
+      assert(result.xml.Articles[0].hasOwnProperty('item'), 'article has no items?');
+      assert((result.xml.Articles[0].item instanceof Array), 'article item is not an array?');
+      assert(result.xml.Articles[0].item[0].hasOwnProperty('Title'), 'article item not containing title?');
+      assert(result.xml.Articles[0].item[0].hasOwnProperty('Description'), 'article item not containing description?');
+      assert(result.xml.Articles[0].item[0].hasOwnProperty('PicUrl'), 'article item not containing pic_url?');
+      assert(result.xml.Articles[0].item[0].hasOwnProperty('Url'), 'article item not containing url?');
+      assert.strictEqual(result.xml.Articles[0].item.length, 5, 'wrong number of stores?');
+      assert((result.xml.Articles[0].item[0].Title instanceof Array), 'article item title is not an array?');
+      assert.strictEqual(result.xml.Articles[0].item[0].Title.join(''), '佛山大良专卖店', 'wrong calculation?');
+      done();
+    });
+  }
+
   describe('when user sends his/her coordinates', function(){
     it('should return a list of nearby stores', function(done){
-      var x = 23.134521, y = 113.358803;
-
-      var store = require('../lib/store');
-      // remove stores.json from module caches as its content was manipulated. it will be 're-required'.
-      delete require.cache[require.resolve('../stores.json')];
-      var stores_list = store.flatten(require('../stores.json'));
-      var list = store.find_nearby_stores_to_weixin_list(stores_list, x, y);
-
       make_request()
         .send(format(test.find_nearby_stores, to, from, x, y))
         .expect(200)
@@ -67,7 +88,23 @@ describe('find nearby stores with position/coordinates functionality', function(
         }, list))
         .end(function(err, res){
           if (err) throw err;
-          done();
+          validate_content(res.text, done);
+        });
+    });
+  });
+
+  describe('when user send coordinates text', function(){
+    it('should return a list of stores near that point', function(done){
+      make_request()
+        .send(format(test.text, to, from, x + ', ' + y))
+        .expect(200)
+        .expect(libweixin.respond_with_list({
+          FromUserName: from,
+          ToUserName: to
+        }, list))
+        .end(function(err, res){
+          if (err) throw err;
+          validate_content(res.text, done);
         });
     });
   });
