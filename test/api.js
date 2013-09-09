cc9d17d2e215783bc9106974ed62ac38e7688849
caiguanhao <caiguanhao@gmail.com>
2013-09-09 17:01:11 +0800

    add /api/stores test; fix bug;

--- test/api.js
+++ test/api.js
@@ -4,6 +4,22 @@ var app = require('../app');
 var request = require('supertest');
 var assert = require('assert');
 
+describe('given nothing', function(){
+  it('should return all stores', function(done){
+    request(app)
+      .get('/api/stores')
+      .expect(200)
+      .end(function(err, res){
+        if (err) throw err;
+        var body = res.body;
+        assert(body.hasOwnProperty('stores'), 'not stores?');
+        assert((body.stores instanceof Array), 'no stores?');
+        assert.strictEqual(body.stores.length, 10, 'wrong number of stores?');
+        done();
+      });
+  });
+});
+
 describe('given latitude and longitutde', function(){
   it('should return a list of nearby stores', function(done){
     var count = 5;
