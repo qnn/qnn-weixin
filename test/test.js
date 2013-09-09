6dff5ae0e2e34d6c2a93349172800f5bb75c2d0a
caiguanhao <caiguanhao@gmail.com>
2013-09-09 08:27:06 +0800

    add find nearby stores response and test;

--- test/test.js
+++ test/test.js
@@ -4,6 +4,8 @@ var app = require('../app');
 var request = require('supertest');
 var libweixin = require('../lib/weixin');
 
+var format = require('util').format;
+
 require('js-yaml');
 var test = require('./test.yml');
 var weixin = require('../lib/weixin.yml');
@@ -18,7 +20,7 @@ var make_request = function() {
 describe('when user subscribe weixin account', function(){
   it('should return a welcome message (aka "newly_subscribed")', function(done){
     make_request()
-      .send(require('util').format(test.subscribe, to, from))
+      .send(format(test.subscribe, to, from))
       .expect(200)
       .expect(libweixin.respond_with_text({
         FromUserName: from,
@@ -34,8 +36,24 @@ describe('when user subscribe weixin account', function(){
 describe('when user unsubscribe weixin account', function(){
   it('should return 204', function(done){
     make_request()
-      .send(require('util').format(test.unsubscribe, to, from))
+      .send(format(test.unsubscribe, to, from))
       .expect(204)
+      .end(function(err, res){
+        if (err) throw err;
+        done();
+      });
+  });
+});
+
+describe('when user sends his/her coordinates', function(){
+  it('should return a list of nearby stores', function(done){
+    make_request()
+      .send(format(test.find_nearby_stores, to, from))
+      .expect(200)
+      .expect(libweixin.respond_with_list({
+        FromUserName: from,
+        ToUserName: to
+      }, [[1,2,3,4]]))
       .end(function(err, res){
         if (err) throw err;
         done();
