5770dc62b2c8be7056270db84812d73ca9f668a6
caiguanhao <caiguanhao@gmail.com>
2013-09-09 12:55:07 +0800

    clean up; group tests;

--- test/test.js
+++ test/test.js
@@ -17,54 +17,58 @@ var make_request = function() {
   return request(app).post('/weixin').set('Content-Type', 'text/xml').query(test.post_query);
 };
 
-describe('when user subscribe weixin account', function(){
-  it('should return a welcome message (aka "newly_subscribed")', function(done){
-    make_request()
-      .send(format(test.subscribe, to, from))
-      .expect(200)
-      .expect(libweixin.respond_with_text({
-        FromUserName: from,
-        ToUserName: to
-      }, config.newly_subscribed))
-      .end(function(err, res){
-        if (err) throw err;
-        done();
-      });
+describe('subscription functionality', function(){
+  describe('when user subscribe weixin account', function(){
+    it('should return a welcome message (aka "newly_subscribed")', function(done){
+      make_request()
+        .send(format(test.subscribe, to, from))
+        .expect(200)
+        .expect(libweixin.respond_with_text({
+          FromUserName: from,
+          ToUserName: to
+        }, config.newly_subscribed))
+        .end(function(err, res){
+          if (err) throw err;
+          done();
+        });
+    });
+  });
+
+  describe('when user unsubscribe weixin account', function(){
+    it('should return 204', function(done){
+      make_request()
+        .send(format(test.unsubscribe, to, from))
+        .expect(204)
+        .end(function(err, res){
+          if (err) throw err;
+          done();
+        });
+    });
   });
 });
 
-describe('when user unsubscribe weixin account', function(){
-  it('should return 204', function(done){
-    make_request()
-      .send(format(test.unsubscribe, to, from))
-      .expect(204)
-      .end(function(err, res){
-        if (err) throw err;
-        done();
-      });
-  });
-});
+describe('find nearby stores with position/coordinates functionality', function(){
+  describe('when user sends his/her coordinates', function(){
+    it('should return a list of nearby stores', function(done){
+      var x = 23.134521, y = 113.358803;
 
-describe('when user sends his/her coordinates', function(){
-  it('should return a list of nearby stores', function(done){
-    var x = 23.134521, y = 113.358803;
+      var store = require('../lib/store');
+      // remove stores.json from module caches as its content was manipulated. it will be 're-required'.
+      delete require.cache[require.resolve('../stores.json')];
+      var stores_list = store.flatten(require('../stores.json'));
+      var list = store.find_nearby_stores_to_weixin_list(stores_list, x, y);
 
-    var store = require('../lib/store');
-    // remove stores.json from module caches as its content was manipulated. it will be 're-required'.
-    delete require.cache[require.resolve('../stores.json')];
-    var stores_list = store.flatten(require('../stores.json'));
-    var list = store.find_nearby_stores_to_weixin_list(stores_list, x, y);
-
-    make_request()
-      .send(format(test.find_nearby_stores, to, from, x, y))
-      .expect(200)
-      .expect(libweixin.respond_with_list({
-        FromUserName: from,
-        ToUserName: to
-      }, list))
-      .end(function(err, res){
-        if (err) throw err;
-        done();
-      });
+      make_request()
+        .send(format(test.find_nearby_stores, to, from, x, y))
+        .expect(200)
+        .expect(libweixin.respond_with_list({
+          FromUserName: from,
+          ToUserName: to
+        }, list))
+        .end(function(err, res){
+          if (err) throw err;
+          done();
+        });
+    });
   });
 });
