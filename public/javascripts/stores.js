dd97a5bd1e8780a097b966a66f2c1b6c223a7da8
caiguanhao <caiguanhao@gmail.com>
2013-09-15 12:45:05 +0800

    lazy load list;

--- public/javascripts/stores.js
+++ public/javascripts/stores.js
@@ -1,12 +1,28 @@
 $(function(){
   if ($('#stores_list_all').length==1) {
-    $.getJSON('/api/stores', { count: 500 }, function(data){
-      $('#stores_list_all tbody').empty();
-      $.each(data.stores, function(a, b){
-        var name = '<a target="_blank" href="/stores/' + b[0] + '">' + b[0] + '</a>';
-        $('#stores_list_all tbody').append('<tr><td>' + (a + 1) + '</td><td>' +
-          name + '<br><small>' + b[1] + ', ' + b[2] + ', ' + b[3] + ', ' + b[4] +
-          '</small></td><td>' + b[9].toFixed(6) + ', ' + b[10].toFixed(6) + '</td></tr>');
+    var start = 0;
+    var step = 30;
+    var load = function(done) {
+      $.getJSON('/api/stores', { count: step, start: start }, function(data){
+        if (start == 0) $('#stores_list_all tbody').empty();
+        $.each(data.stores, function(a, b){
+          var name = '<a target="_blank" href="/stores/' + b[0] + '">' + b[0] + '</a>';
+          $('#stores_list_all tbody').append('<tr><td>' + (start + a + 1) + '</td><td>' +
+            name + '<br><small>' + b[1] + ', ' + b[2] + ', ' + b[3] + ', ' + b[4] +
+            '</small></td><td>' + b[9].toFixed(6) + ', ' + b[10].toFixed(6) + '</td></tr>');
+        });
+        done(data.stores.length);
+      });
+    };
+    load();
+    $('#load_more').click(function(){
+      $('#load_more').prop('disabled', true);
+      start += step;
+      load(function(count){
+        $('#load_more').prop('disabled', false);
+        if (count < step) {
+          $('#stores_list_all tfoot').hide();
+        }
       });
     });
   }
